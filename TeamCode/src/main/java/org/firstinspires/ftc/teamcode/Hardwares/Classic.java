841fac211befdaa276b87c2105a01c6899a936c0
Benny0Li <bennyli2011@163.com>
2024-08-02 14:54:47 +0800

    update Mathematics

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardwares/Classic.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardwares/Classic.java
@@ -6,6 +6,7 @@ import androidx.annotation.NonNull;
 
 import org.firstinspires.ftc.teamcode.Hardwares.basic.Motors;
 import org.firstinspires.ftc.teamcode.RuntimeOption;
+import org.firstinspires.ftc.teamcode.utils.Mathematics;
 import org.firstinspires.ftc.teamcode.utils.enums.place;
 import org.firstinspires.ftc.teamcode.utils.enums.driveDirection;
 
@@ -99,11 +100,10 @@ public class Classic {
 	}
 
 	/**
-	 * @param angle 相较于机器的正方向，允许为[-180,180]内的实数
+	 * @param angle 相较于机器的正方向，允许为[-180,180]内的实数(不是弧度，不是弧度，不是弧度）
 	 */
 	public void SimpleDrive(double power,double angle){
-		while (angle<=-180)angle+=360;
-		while (angle>180)angle-=360;
+		angle= Mathematics.angleRationalize(angle);
 
 		if(angle==0){
 			drive(driveDirection.forward,power);
@@ -123,6 +123,30 @@ public class Classic {
 			drive(driveDirection.slant,place.B,power,90+angle);
 		}else if(angle>-180&&angle<0){//第三象限
 			drive(driveDirection.slant,place.C,power,-90-angle);
+		}
+	}
+
+	public void SimpleRadiansDrive(double power,double radians){
+		radians=Mathematics.radiansRationalize(radians);
+
+		if(radians==0){
+			drive(driveDirection.forward,power);
+		}else if(radians==Math.PI/2){
+			drive(driveDirection.right,power);
+		}else if(radians==-Math.PI/2){
+			drive(driveDirection.left,power);
+		}else if(radians==Math.PI){
+			drive(driveDirection.back,power);
+		}
+
+		if(radians>0&&radians<Math.PI/2){//第一象限
+			drive(driveDirection.slant,place.A,power,Math.toRadians(Math.PI/2-radians));
+		}else if(radians>Math.PI/2&&radians<Math.PI){//第四象限
+			drive(driveDirection.slant,place.D,power,Math.toRadians(radians-Math.PI/2));
+		}else if(radians>-Math.PI/2&radians<0){//第二象限
+			drive(driveDirection.slant,place.B,power,Math.toRadians(Math.PI/2+radians));
+		}else if(radians>-Math.PI&&radians<0){//第三象限
+			drive(driveDirection.slant,place.C,power,Math.toRadians(-Math.PI/2-radians));
 		}
 	}
 }
