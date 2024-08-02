8e639a58c90fc63ca7b6df22b6512167f4a12d52
Benny0Li <bennyli2011@163.com>
2024-08-02 14:22:44 +0800

    Create RoboticActions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoboticActions.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.utils.enums.runningState;
+
+public class RoboticActions extends Robot{
+	public RoboticActions(HardwareMap hardwareMap, runningState state) {
+		super(hardwareMap, state);
+	}
+}
