0e3e2b61a3b261876d5b5c6d0e1aae8ec0dcb29e
Benny0Li <bennyli2011@163.com>
2024-08-15 09:31:44 +0800

    实现用shell压缩文件

--- zipper.sh
+++ zipper.sh
@@ -1,3 +1,7 @@
+zipperParentPathName=$(dirname "$0")
+programPath=TeamCode/src/main/java/org/firstinspires/ftc/teamcode
+cd ${programPath} || exit
 rm -i RIC.zip
-programRoot=TeamCode/src/main/java/org/firstinspires/ftc/teamcode
-zip -r RIC.zip ${programRoot}/DriveControlsAddition/ ${programRoot}/utils/ ${programRoot}/RIC_samples/ ${programRoot}/Hardwares/
+zip -r RIC.zip DriveControlsAddition/ utils/ RIC_samples/ Hardwares/
+cd "${zipperParentPathName}" || exit
+mv ${programPath}/RIC.zip "${zipperParentPathName}"
