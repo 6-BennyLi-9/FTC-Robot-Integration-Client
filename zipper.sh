666ef602b4cb98cf42c77c602e61cbaf33bba926
Benny0Li <bennyli2011@163.com>
2024-08-21 21:09:41 +0800

    更新 zipper.sh

--- zipper.sh
+++ zipper.sh
@@ -1,5 +1,9 @@
 zipperParentPathName=$(dirname "$0")
 programPath=TeamCode/src/main/java/org/firstinspires/ftc/teamcode
+
+echo ParentPath "${zipperParentPathName}"
+echo ProgramPath "${programPath}"
+
 cd ${programPath} || exit
 rm -i RIC.zip
 zip -r RIC.zip ./
