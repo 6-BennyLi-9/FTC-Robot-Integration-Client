b66ef6dd04c4dbc9e89dc4e836cb9a2cb1dc7ac6
Benny0Li <bennyli2011@163.com>
2024-08-22 21:31:31 +0800

    加入 TeleopProgramTemplate

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/TeleopProgramTemplate.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.utils;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.utils.enums.runningState;
+
+public abstract class TeleopProgramTemplate extends OpMode {
+	public Robot robot;
+	public long st,et;
+	@Override
+	public void init() {
+		robot=new Robot(hardwareMap, runningState.ManualDrive,telemetry);
+	}
+
+	@Override
+	public void start() {
+		super.start();
+		st=System.currentTimeMillis();
+		robot.client.addData("TPS","WAIT FOR START");
+	}
+
+	@Override
+	public void loop() {
+		et=System.currentTimeMillis();
+		robot.client.changeDate("TPS", (double) (et - st) /1000);
+		st=et;
+
+		whileActivating();
+	}
+	public void whileActivating(){}
+}
