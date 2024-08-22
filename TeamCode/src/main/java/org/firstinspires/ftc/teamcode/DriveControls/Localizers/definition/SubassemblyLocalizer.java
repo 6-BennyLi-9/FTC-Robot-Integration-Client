76e431d83bad4592fdc7aef823edf3c7e39a3bb3
Benny0Li <bennyli2011@163.com>
2024-08-22 10:39:36 +0800

    优化 SubassemblyLocalizer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveControls/Localizers/definition/SubassemblyLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveControls/Localizers/definition/SubassemblyLocalizer.java
@@ -4,55 +4,55 @@ import androidx.annotation.NonNull;
 
 import com.acmerobotics.roadrunner.Pose2d;
 
-import org.firstinspires.ftc.teamcode.utils.enums.LocalizerSubassemblyType;
-
 public abstract class SubassemblyLocalizer implements Localizer{
-	public final LocalizerSubassemblyType type;
 	public final LocalizerPlugin[] plugins;
 	public Pose2d RobotPosition;
 
 	public SubassemblyLocalizer(PositionLocalizerPlugin localizerPlugin){
 		plugins=new LocalizerPlugin[]{localizerPlugin};
-		type=LocalizerSubassemblyType.SingleLocalizer;
 	}
 	public SubassemblyLocalizer(VectorPositionLocalizerPlugin localizerPlugin1, HeadingLocalizerPlugin localizerPlugin2){
 		plugins=new LocalizerPlugin[]{localizerPlugin1,localizerPlugin2};
-		type=LocalizerSubassemblyType.SubassemblyLocalizer;
 	}
 	public SubassemblyLocalizer(@NonNull LocalizerPlugin[] localizerClasses){
-		type=LocalizerSubassemblyType.SynthesisLocalizer;
 		plugins=localizerClasses;
 	}
 
 	public void update(){
-		switch (type) {
-			case SingleLocalizer:
-				plugins[0].update();
-				RobotPosition=((PositionLocalizerPlugin) plugins[0]).getCurrentPose();
-				break;
-			case SubassemblyLocalizer:
-				plugins[0].update();
-				plugins[1].update();
-				RobotPosition=new Pose2d(
-						((VectorPositionLocalizerPlugin) plugins[0]).getCurrentVector(),
-						((HeadingLocalizerPlugin) plugins[1]).getHeadingDeg()
-				);
-				break;
-			case SynthesisLocalizer:
-				Pose2d cache=new Pose2d(0,0,0);
-				for(LocalizerPlugin plugin:plugins){
-					cache=new Pose2d(
-							cache.position.x+((PositionLocalizerPlugin)plugin).getCurrentPose().position.x,
-							cache.position.y+((PositionLocalizerPlugin)plugin).getCurrentPose().position.y,
-							cache.heading.toDouble()+((PositionLocalizerPlugin)plugin).getCurrentPose().heading.toDouble()
-					);
-				}
+		Pose2d cache=new Pose2d(0,0,0);
+		int VectorFactor=0,HeadingFactor=0;
+		for(LocalizerPlugin plugin:plugins){
+			plugin.update();
+			if(plugin instanceof VectorPositionLocalizerPlugin){
 				cache=new Pose2d(
-						cache.position.x/plugins.length,
-						cache.position.y/plugins.length,
-						cache.heading.toDouble()/plugins.length
+						cache.position.x+((VectorPositionLocalizerPlugin)plugin).getCurrentVector().x,
+						cache.position.y+((VectorPositionLocalizerPlugin)plugin).getCurrentVector().y,
+						cache.heading.toDouble()
 				);
-				RobotPosition=cache;
+				++VectorFactor;
+			}else if(plugin instanceof HeadingLocalizerPlugin){
+				cache=new Pose2d(
+						cache.position,
+						cache.heading.toDouble()+Math.toRadians(((HeadingLocalizerPlugin)plugin).getHeadingDeg())
+				);
+				++HeadingFactor;
+			}else if(plugin instanceof PositionLocalizerPlugin){
+				cache=new Pose2d(
+						cache.position.x+((PositionLocalizerPlugin)plugin).getCurrentPose().position.x,
+						cache.position.y+((PositionLocalizerPlugin)plugin).getCurrentPose().position.y,
+						cache.heading.toDouble()+((PositionLocalizerPlugin)plugin).getCurrentPose().heading.toDouble()
+				);
+				++VectorFactor;
+				++HeadingFactor;
+			}else{
+				throw new RuntimeException("unknown localizer plugin");
+			}
 		}
+		cache=new Pose2d(
+				cache.position.x/VectorFactor,
+				cache.position.y/VectorFactor,
+				cache.heading.toDouble()/HeadingFactor
+		);
+		RobotPosition=cache;
 	}
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/enums/LocalizerSubassemblyType.java
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode.utils.enums;
-
-public enum LocalizerSubassemblyType {
-	SingleLocalizer,
-	SubassemblyLocalizer,
-	SynthesisLocalizer
-}
