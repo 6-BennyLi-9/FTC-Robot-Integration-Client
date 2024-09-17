6f34149a89d3bf776740c75248bd2b4917a72153
BennyLi <bennyli2011@163.com>
2024-09-17 18:29:46 +0800

    加入 ConstantAccelMath, IntegralAutoCorrection, SimplePosition

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/SimplePosition.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.Utils;
+
+public class SimplePosition {
+	public double x,y, heading;
+	public SimplePosition(double x,double y,double heading){
+		this.x=x;
+		this.y=y;
+		this.heading = heading;
+	}
+}
