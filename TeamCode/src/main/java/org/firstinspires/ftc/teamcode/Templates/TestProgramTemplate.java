92a7f09718d8fc834d7cf58a6345059a09304ca1
BennyLi <bennyli2011@163.com>
2024-09-21 12:53:35 +0800

    Add DebugProgramTemplate, TestProgramTemplate, TuningProgramTemplate

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Templates/TestProgramTemplate.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.Templates;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Utils.Clients.Client;
+
+public abstract class TestProgramTemplate extends LinearOpMode {
+	public Client client;
+
+	@Override
+	public void runOpMode() throws InterruptedException {
+		client=new Client(telemetry);
+		opInit();
+
+
+		while(!opModeIsActive()&&!isStopRequested()){
+			sleep(500);
+		}
+
+		if(isStopRequested())return;
+
+		mainCode();
+		sleep(1145141919);
+	}
+
+	public abstract void opInit();
+	public abstract void mainCode();
+}
