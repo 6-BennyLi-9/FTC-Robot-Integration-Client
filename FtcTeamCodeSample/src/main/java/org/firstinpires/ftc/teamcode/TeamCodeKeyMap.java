8dce24b091f54551ba8ddd5c8a8363127703f896
BennyLi <bennyli2011@163.com>
2024-09-28 14:55:15 +0800

    注释

--- FtcTeamCodeSample/src/main/java/org/firstinpires/ftc/teamcode/TeamCodeKeyMap.java
+++ FtcTeamCodeSample/src/main/java/org/firstinpires/ftc/teamcode/TeamCodeKeyMap.java
@@ -2,9 +2,6 @@ package org.firstinpires.ftc.teamcode;
 
 import androidx.annotation.NonNull;
 
-import org.firstinspires.ftc.teamcode.Hardwares.Integration.Gamepad.KeyButtonType;
-import org.firstinspires.ftc.teamcode.Hardwares.Integration.Gamepad.KeyMapSettingType;
-import org.firstinspires.ftc.teamcode.Hardwares.Integration.Gamepad.KeyTag;
 import org.firstinspires.ftc.teamcode.KeyMap;
 import org.firstinspires.ftc.teamcode.KeyMapController;
 
@@ -15,6 +12,6 @@ public class TeamCodeKeyMap extends KeyMapController {
 	@Override
 	public void KeyMapOverride(@NonNull KeyMap keyMap) {
 		//TODO:填入参数，格式如下
-		keyMap.loadButtonContent(KeyTag.Intake, KeyButtonType.X, KeyMapSettingType.SinglePressToChangeRunAble);
+//		keyMap.loadButtonContent(KeyTag.Intake, KeyButtonType.X, KeyMapSettingType.SinglePressToChangeRunAble);
 	}
 }
