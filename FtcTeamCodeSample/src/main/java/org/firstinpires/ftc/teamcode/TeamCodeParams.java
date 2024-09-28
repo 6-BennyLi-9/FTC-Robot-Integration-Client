e81ffbea7e103ae19e37bd99fbd5fcdc6ed651bb
BennyLi <bennyli2011@163.com>
2024-09-28 14:11:08 +0800

    加入 FtcTeamCodeSample

---
+++ FtcTeamCodeSample/src/main/java/org/firstinpires/ftc/teamcode/TeamCodeParams.java
@@ -0,0 +1,17 @@
+package org.firstinpires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.config.Config;
+
+import org.firstinspires.ftc.teamcode.ParamsController;
+
+/**
+ * 可以将该类重命名为<code><队伍编号>Params</code>
+ */
+@Config
+public class TeamCodeParams extends ParamsController {
+	@Override
+	public void PramsOverride() {
+		//TODO:填入参数，格式如下
+//		Params.AxialInchPerTick=114;
+	}
+}
