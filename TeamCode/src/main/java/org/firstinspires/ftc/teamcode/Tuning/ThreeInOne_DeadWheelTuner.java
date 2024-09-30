c68e07fa81aa891ed56755190ca2f1028d93b04c
BennyLi <bennyli2011@163.com>
2024-09-30 21:29:49 +0800

    Debug

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tuning/ThreeInOne_DeadWheelTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tuning/ThreeInOne_DeadWheelTuner.java
@@ -15,6 +15,16 @@ public class ThreeInOne_DeadWheelTuner extends TuningProgramTemplate {
 	@Override
 	public void whenInit() {
 		robot.addLine("该调参程序不具备定位和直接数据处理能力，以下所能看到的辅助调参数据都是直接相乘得到的，因此不具备定位能力！！！");
+
+		robot.addData("TurningDegPerTick","wait for start");
+		robot.addData("AxialInchPerTick","wait for start");
+		robot.addData("LateralInchPerTick","wait for start");
+
+		robot.addLine("如果你已经填入了Params数据，则在这里将会直接得到相乘结果");
+
+		robot.addData("TurningDegPerTick","wait for start");
+		robot.addData("AxialInchPerTick","wait for start");
+		robot.addData("LateralInchPerTick","wait for start");
 	}
 
 	@Override
@@ -22,8 +32,6 @@ public class ThreeInOne_DeadWheelTuner extends TuningProgramTemplate {
 		robot.changeData("TurningDegPerTick",robot.sensors.getDeltaT());
 		robot.changeData("AxialInchPerTick",robot.sensors.getDeltaA());
 		robot.changeData("LateralInchPerTick",robot.sensors.getDeltaL());
-
-		robot.addLine("如果你已经填入了Params数据，则在这里将会直接得到相乘结果");
 
 		robot.changeData("TurningDegPerTick",robot.sensors.getDeltaT() * Params.TurningDegPerTick);
 		robot.changeData("AxialInchPerTick",robot.sensors.getDeltaA() * Params.AxialInchPerTick);
