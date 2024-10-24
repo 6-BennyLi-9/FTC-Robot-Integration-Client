ac8588e081ae7b2e574bf138458d064ea720132a
BennyLi <bennyli2011@163.com>
2024-10-24 20:37:17 +0800

    加入更多针对 Dashboard 的调试程序

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/codes/tunings/DashboardTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/codes/tunings/DashboardTest.java
@@ -1,24 +1,31 @@
 package org.firstinspires.ftc.teamcode.codes.tunings;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.Params;
 import org.firstinspires.ftc.teamcode.codes.templates.TuningProgramTemplate;
 import org.firstinspires.ftc.teamcode.utils.Functions;
-import org.firstinspires.ftc.teamcode.utils.Position2d;
-import org.firstinspires.ftc.teamcode.utils.Vector2d;
-import org.firstinspires.ftc.teamcode.utils.clients.DashboardClient;
+import org.firstinspires.ftc.teamcode.utils.Timer;
 
-@Autonomous(name = "DashboardTest",group = Params.Configs.TuningAndTuneOpModesGroup)
+@Autonomous(name = "DashboardTest" , group = Params.Configs.TuningAndTuneOpModesGroup)
 public class DashboardTest extends TuningProgramTemplate {
+	public TelemetryPacket packet;
+	public FtcDashboard dashboard;
+
 	@Override
 	public void whenInit() {
-		client.dashboard.DrawLine(new Vector2d(1,1),new Vector2d(2,2));
-		timer.restart();
+		packet=new TelemetryPacket();
+		dashboard=FtcDashboard.getInstance();
 	}
 
 	@Override
 	public void whileActivating() {
-		client.dashboard.DrawRobot(new Position2d(0, Functions.roundClip(timer.restartAndGetDeltaTime(),64),114), DashboardClient.Blue,"aim");
+		packet.fieldOverlay().strokeLine(
+				0,0,
+				Functions.roundClip(Timer.getCurrentTime(),10),Functions.roundClip(Timer.getCurrentTime(),10)
+		);
+		dashboard.sendTelemetryPacket(packet);
 	}
 }
