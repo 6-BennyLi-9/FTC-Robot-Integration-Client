2cdfc4bed5c01b672f3e6a1fa601d8aab54683c4
Benny0Li <17826002105@163.com>
2024-10-31 12:11:17 +0800

    加入 DashboardTelemetryTest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/codes/tunings/DashboardTelemetryTest.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.codes.tunings;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.Params;
+import org.firstinspires.ftc.teamcode.codes.templates.TuningProgramTemplate;
+import org.firstinspires.ftc.teamcode.utils.Timer;
+
+@Autonomous(name = "DashboardTelemetryTest",group = Params.Configs.TuningAndTuneOpModesGroup)
+public class DashboardTelemetryTest extends TuningProgramTemplate {
+	@Override
+	public void whenInit() {
+
+	}
+
+	@Override
+	public void whileActivating() {
+		client.changeData("time", Timer.getCurrentTime());
+		client.dashboard.sendPacket();
+	}
+}
